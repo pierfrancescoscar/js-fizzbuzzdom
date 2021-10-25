44e07f4cce31afa3a4132b6f29306c52c011fc72
pierfrancescoscar <scarinci401@gmail.com>
2021-10-25 16:16:25 +0200

    add cycle "for" for multiple of 3 and 5 with style

--- js/main.js
+++ js/main.js
@@ -2,14 +2,30 @@
 
 // Ref
 
-const fizzBoxes = document.querySelector('.fizzBoxes')
+const fizzBoxes = document.querySelector('.fizzBoxes');
 
 // Creazione ciclo da 1 a 100
 
 for (let i=1; i <= 100; i++) {
+
     fizzBoxes.innerHTML += `<div class="fizzBox fizzBox-${i}">${i}</div>`;
+
+    // Diversi stili per multipli di 5 e 3, multipli di 3, multipli di 5
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        fizzBoxes.innerHTML = `<div class="fizzBox fizzBox-${i} fizzbuzz">fizzbuzz</div>`;
+    }
+
+    else if (i % 3 == 0) {
+        fizzBoxes.innerHTML = `<div class="fizzBox fizzBox-${i} fizz">fizz</div>`;
+        }
+
+    else if(i % 5 == 0) {
+        fizzBoxes.innerHTML = `<div class="fizzBox fizzBox-${i} buzz">buzz</div>`;
+        }
 }
 
+   
 
 
 
